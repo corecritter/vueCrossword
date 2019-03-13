cd18ca2f84dc5e0da27819b259ecf8294b18fa50
Unknown <bobbysavel@gmail.com>
2019-03-13 17:46:36 -0400

    Fix compiler complaints

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { getStoreAccessors } from 'vuex-typescript';
-import * as Vuex from 'vuex';
+import { getStoreAccessors } from 'vuex-typescript'
+import * as Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import TestComponent from './components/TestComponent.vue'
 
 Vue.use(VueRouter)
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 interface InputState {
@@ -22,46 +22,42 @@ type DataContext = Vuex.ActionContext<InputState, RootState>;
 const data = {
   namespaced: true,
   state: {
-    InputWords: "t1, t2"
+    InputWords: 't1, t2'
   },
   getters: {
-    GetInputWords(state: InputState ) : string {
-      return state.InputWords as string;
+    GetInputWords (state: InputState) : string {
+      return state.InputWords as string
     }
   },
   mutations: {
-    setInputWords(state: InputState, newValue: string) {
-      console.log('setWordsAction triggered with', newValue);
-      state.InputWords = newValue;
+    setInputWords (state: InputState, newValue: string) {
+      console.log('setWordsAction triggered with', newValue)
+      state.InputWords = newValue
     }
   },
   actions: {
 
   }
-};
+}
 
-export const InputModule: Vuex.Module<InputState, RootState> = data;
+export const InputModule: Vuex.Module<InputState, RootState> = data
 
-const { commit, read, dispatch } = getStoreAccessors("InputModule");
+const { commit, read, dispatch } = getStoreAccessors('InputModule')
 
-const getters = data.getters;
+const getters = data.getters
 
-export const AllInputWords = read(getters.GetInputWords);
+export const AllInputWords = read(getters.GetInputWords)
 
-export const SetInputWords = commit(data.mutations.setInputWords);
+export const SetInputWords = commit(data.mutations.setInputWords)
 
 export const store = new Vuex.Store<RootState>({
   state: {
-    version: "1.0"
+    version: '1.0'
   },
   modules: {
     InputModule
   }
 })
-
-
-
-
 
 const routes = [
   { path: 'test', component: TestComponent }
@@ -70,7 +66,6 @@ const routes = [
 const router = new VueRouter({
   routes // short for `routes: routes`
 })
-
 
 new Vue({
   router,
