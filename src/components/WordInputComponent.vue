f7d58a135b6406a03925e56e642ef149613f89b9
Unknown <bobbysavel@gmail.com>
2019-03-13 21:00:52 -0400

    Kind of ugly but she's came a long way

--- src/components/WordInputComponent.vue
+++ src/components/WordInputComponent.vue
@@ -1,22 +1,38 @@
 <template>
-  <div class="hello">
+  <div class="root">
     <p>
-      Enter your crossword input in the JSON format like this:
+      Enter your crossword input in JSON format like this:
     </p>
+    <div class="json-pretty">
+      <vue-json-pretty
+        :data="jsonDemoString">
+      </vue-json-pretty>
+    </div>
     <input v-model="input_words" placeholder="enter the words"/>
+    <br>
     <button v-on:click="generate_click">Generate!</button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import VueJsonPretty from 'vue-json-pretty'
 import TestComponent from './TestComponent.vue'
 import { AllInputWords, SetInputWords } from '../main'
 
 export default Vue.extend({
   name: 'app',
   components: {
-    TestComponent
+    TestComponent,
+    VueJsonPretty
+  },
+  data () {
+    return {
+      jsonDemoString: [
+          {word:'Working', hint:'You are probably not doing this right now'},
+          {word:'Daydream', hint:'You are probably doing this right now'}
+      ]
+    }
   },
   computed: {
     input_words: {
@@ -40,6 +56,15 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.root {
+  margin: auto;
+  width: 60%;
+  border: 1px solid #73AD21;
+  padding: 10px;
+}
+.json-pretty {
+  text-align: left;
+}
 h3 {
   margin: 40px 0 0;
 }
