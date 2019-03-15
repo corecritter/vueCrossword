f4f231c7726857bd0498c7f7dfc1102fbb761ea1
Unknown <bobbysavel@gmail.com>
2019-03-15 00:08:47 -0400

    Show the numbers

--- src/components/crossword/CrosswordCellComponent.vue
+++ src/components/crossword/CrosswordCellComponent.vue
@@ -1,15 +1,28 @@
 <template>
     <div class="cell-style">
         <div v-if="cell.value">
-            <div v-if="show_answer" class="space-style">
-                {{cell.value}}
-            </div>
-            <div v-else class="space-style">
-                -
+            <div class="root-space-style">
+                <div v-if="show_answer" class="letter-style">
+                    {{cell.value}}
+                </div>
+                <div v-else class="letter-style">
+                    -
+                </div>
+                <div v-if="cell.cellNumber" class="number-style">
+                    {{cell.cellNumber}}
+                </div>
+                <div v-else class="number-hidden-style">
+                    -
+                </div>
             </div>
         </div>
-        <div v-else class="fill-style">
-             _  
+        <div v-else class="root-fill-style">
+            <div class="fill-style">
+                 _
+            </div>
+            <div class="small-fill-style">
+                _
+            </div>
         </div>
     </div>
 </template>
@@ -37,18 +50,52 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-    .space-style {
+    .root-space-style {
+        display:flex;
+        flex-direction: column;
         background-color: rgba(255, 255, 255, 0.8);
         border: 1px solid rgba(0, 0, 0, 0.8);
-        padding: 20px;
+    }
+    .letter-style {
+        flex:1;
+        text-align:center;
         font-size: 30px;
-        text-align: center;
+    }
+    .number-style {
+        flex: .1;
+        font-size: 3px;
+        text-align: left;
+        color:black;
+        font-weight: bold;
+        font-size: 12px;
+        padding: 5px;
+    }
+    .number-hidden-style {
+        flex: .1;
+        font-size: 3px;
+        text-align: left;
+        color:white;
+        font-weight: bold;
+        font-size: 12px;
+        padding: 5px;
+    }
+    .root-fill-style {
+        display:flex;
+        flex-direction: column;
+        background-color: black;
+        border: 1px solid rgba(0, 0, 0, 0.8);
     }
     .fill-style {
+        flex: 1;
+        color: black;
+        font-size: 30px;
+    }
+    .small-fill-style {
+        flex: .1;
         background-color: black;
         color: black;
-        border: 1px solid rgba(0, 0, 0, 0.8);
-        padding: 20px;
-        font-size: 30px;
+        font-weight: bold;
+        font-size: 12px;
+        padding: 5px;
     }
 </style>
